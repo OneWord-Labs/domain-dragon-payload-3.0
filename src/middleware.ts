d8787a881d11d17f3110f1d9dc81ea5c230848c2
Sahas Prajapati <sahasprajapati@gmail.com>
2024-07-26 00:42:26 +0545

    fix: middleware and redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,16 +25,32 @@ export default async function middleware(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams.toString()
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ''}`
-
   if (path?.startsWith('/api')) {
+    console.log(
+      hostname,
+      `Rewriting ${req.url} to ${new URL(`${process.env.NEXT_PAYLOAD_API_URL}${path}`).toString()}`,
+    )
     return NextResponse.rewrite(new URL(`${process.env.NEXT_PAYLOAD_API_URL}${path}`))
   }
-
   if (path?.startsWith('/admin') && hostname !== `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+    console.log(
+      hostname,
+      `Rewriting ${req.url} to ${new URL(`${process.env.NEXT_PAYLOAD_API_URL}${path}`).toString()}`,
+    )
     return NextResponse.redirect(new URL(`${process.env.NEXT_PAYLOAD_API_URL}${path}`))
   }
   if (!path?.startsWith('/admin') && hostname === `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+    console.log(
+      hostname,
+      `Rewriting ${req.url} to ${new URL(`${process.env.NEXT_PAYLOAD_API_URL}/admin`).toString()}`,
+    )
     return NextResponse.redirect(new URL(`${process.env.NEXT_PAYLOAD_API_URL}/admin`))
+  }
+  if (
+    path === '/' &&
+    (hostname === 'localhost:3000' || hostname === `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`)
+  ) {
+    return NextResponse.rewrite(new URL('https://domaindragon.framer.website/'))
   }
 
   if (
@@ -44,6 +60,7 @@ export default async function middleware(req: NextRequest) {
   ) {
     return NextResponse.rewrite(new URL(req.url))
   }
+
   console.log(
     hostname,
     `Rewriting ${req.url} to ${new URL(`/${hostname}${path}`, req.url).toString()}`,
